a812e15d0455f73eccd1611c9c398ef174489b22
Dave Glover <dglover@microsoft.com>
2024-10-28 03:37:46 +1100

    clean up

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from chainlit.utils import mount_chainlit
 from fastapi import FastAPI
 
 # Map environment to target path
-target = {"development": "src/app.py", "production": "app.py"}.get(os.getenv("ENV", "development"))
+env = os.getenv("ENV", "development")
+target = {"development": "src/app.py", "production": "app.py"}.get(env)
 
 app = FastAPI()
 mount_chainlit(app=app, target=target, path="/sales")
