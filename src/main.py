9e0ba879054fdb5b48cd01a8de2065db8ec1993d
Dave Glover <dglover@microsoft.com>
2024-10-28 03:10:58 +1100

    Update main.py
    clean up

--- src/main.py
+++ src/main.py
@@ -4,25 +4,17 @@ from chainlit.utils import mount_chainlit
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-env = os.getenv('ENV', 'development')
+env = os.getenv("ENV", "development")
 
 # Set the target path based on the environment
-if env == 'development':
+if env == "development":
     target = "src/app.py"
-elif env == 'production':
+elif env == "production":
     target = "app.py"
 else:
     raise ValueError(f"Unknown environment: {env}")
 
 app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Change to specific domains in production
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 
 mount_chainlit(app=app, target=target, path="/sales")
